93f78d2424ba8ff8a9a1addcd3450316996fa94e
Oscar Millot <oscarmillot23@gmail.com>
2021-09-13 11:28:29 +0200

    exo6

--- exercice6/script.js
+++ exercice6/script.js
@@ -1,8 +1,10 @@
+let toto = document.querySelector("button");
+
 window.addEventListener("load", () => {
     document.body.style.background = "black";
 })
 
-window.addEventListener("keydown", (e) => {
+window.addEventListener("keydown", (e) => { 
     if (e.key === 'r') {
         document.body.style.background = "red";
     } else if (e.key === 'g') {
@@ -14,9 +16,26 @@ window.addEventListener("keydown", (e) => {
     } else if (e.key === 'y') {
         document.body.style.background = "yellow";
     } else if (e.key === 'ArrowUp') {
-        document.body.style.background = "yellow";
+        toto.style.display = "block";
     } else {
         document.body.style.background = "white";
     }
-    console.log(e.key);
+})
+
+toto.addEventListener("click", () => {
+
+    let userDate = prompt("Entrez votre date de naissance (dd/mm/yyyy)");
+    let date = userDate.split("/");
+    let d1 = new Date();
+    let d2 = new Date(date[2], date[1], date[0]);
+    let days = Math.floor((d1 - d2) / (1000 *60 *60 * 24));
+    if((days/364) >= 25){
+        alert("Bienvenue");
+    } else {
+        let x = ((25 * 364) - days);
+        alert(`Il faut attendre encore ${x} jours`);
+        console.log(x);
+    }
+    // alert("alert - Hello, " + name + "! You can't enter text here!");
+
 })
