72d520c0dac37ebbc52127feeca43e7f2d5c1658
Oscar Millot <oscarmillot23@gmail.com>
2021-09-13 11:59:53 +0200

    deuxième point exo5

--- exercice5/script.js
+++ exercice5/script.js
@@ -1,9 +1,15 @@
 window.addEventListener("load", () => {
-    let h2_1 = document.querySelector("body section:nth-child(2) h2");
-    let p_1 = document.querySelector("body section:nth-child(2) p");
+    const h2_1 = document.querySelector("body section:nth-child(2) h2");
+    const p_1 = document.querySelector("body section:nth-child(2) p");
     const monNom = "Millot";
     const monPrenom = "Oscar";
     h2_1.textContent = `Votre nom est ${monNom}`;
     p_1.textContent = `et votre prenom est ${monPrenom}`;
-    console.log(h2_1.value);
+})
+
+const div_2 = document.querySelector("body section:nth-child(3) div");
+
+div_2.addEventListener("click", () => {
+    div_2.style.borderRadius = "50%";
+    div_2.style.background = "RGB(222, 49, 99)";
 })
