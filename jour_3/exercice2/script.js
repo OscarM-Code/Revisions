8a9f0159f4502a9c3af13e10072cb73a8a753458
Oscar Millot <oscarmillot23@gmail.com>
2021-09-17 09:34:24 +0200

    modif test exo2 j3

--- jour_3/exercice2/script.js
+++ jour_3/exercice2/script.js
@@ -1,13 +1,22 @@
 let seeBtn = document.querySelector("section:nth-child(1) button");
 
-seeBtn.addEventListener("click", async() => {
-    const url = "see.php";
-    const options = {
-        method: "POST"
-    }
+seeBtn.addEventListener("click", () => {
+  loadData();
+});
 
-    const response = await fetch(url, options);
-    // const user = await response.json();
-    
-    console.log(response);
-})
+async function loadData() {
+  const json = [{ name: "Oscar" }];
+  const header = {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+  };
+
+  const response = await fetch(`see.php`, {
+    method: "POST",
+    header: header,
+    body: JSON.stringify(json),
+  });
+  console.log(JSON.stringify(json));
+  const user = await response.text();
+  document.body.innerHTML = user;
+}
